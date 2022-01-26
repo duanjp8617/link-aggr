e4258f0346b6ed3b746fc3f0908f499a22a1e8f1
duanjp8617 <duanjp8617@gmail.com>
2022-01-26 07:12:18 +0000

    update main.cc

--- aggr-client/main.cc
+++ aggr-client/main.cc
@@ -2,50 +2,95 @@
  * Copyright(c) 2010-2014 Intel Corporation
  */
 
-#include <stdio.h>
-#include <string.h>
-#include <stdint.h>
-#include <errno.h>
-#include <sys/queue.h>
+// #include <stdio.h>
+// #include <string.h>
+// #include <stdint.h>
+// #include <errno.h>
+// #include <sys/queue.h>
 
-#include <rte_memory.h>
-#include <rte_launch.h>
-#include <rte_eal.h>
-#include <rte_per_lcore.h>
-#include <rte_lcore.h>
-#include <rte_debug.h>
+// #include <rte_memory.h>
+// #include <rte_launch.h>
+// #include <rte_eal.h>
+// #include <rte_per_lcore.h>
+// #include <rte_lcore.h>
+// #include <rte_debug.h>
 
-static int
-lcore_hello(__rte_unused void *arg)
+// static int
+// lcore_hello(__rte_unused void *arg)
+// {
+// 	unsigned lcore_id;
+// 	lcore_id = rte_lcore_id();
+// 	printf("hello from core %u\n", lcore_id);
+// 	return 0;
+// }
+
+// int
+// main(int argc, char **argv)
+// {
+// 	int ret;
+// 	unsigned lcore_id;
+
+// 	ret = rte_eal_init(argc, argv);
+// 	if (ret < 0)
+// 		rte_panic("Cannot init EAL\n");
+
+// 	/* call lcore_hello() on every worker lcore */
+// 	RTE_LCORE_FOREACH_WORKER(lcore_id) {
+// 		rte_eal_remote_launch(lcore_hello, NULL, lcore_id);
+// 	}
+
+// 	/* call it on main lcore too */
+// 	lcore_hello(NULL);
+
+// 	rte_eal_mp_wait_lcore();
+
+// 	/* clean up the EAL */
+// 	rte_eal_cleanup();
+
+// 	return 0;
+// }
+
+#include <iostream>
+
+struct subflow_header
 {
-	unsigned lcore_id;
-	lcore_id = rte_lcore_id();
-	printf("hello from core %u\n", lcore_id);
-	return 0;
-}
+	uint64_t subflow_idx;
+	uint64_t seq_num;
+};
 
-int
-main(int argc, char **argv)
+class subflow_header_generator
 {
-	int ret;
-	unsigned lcore_id;
+public:
+	subflow_header_generator() : _counter(0) {}
 
-	ret = rte_eal_init(argc, argv);
-	if (ret < 0)
-		rte_panic("Cannot init EAL\n");
-
-	/* call lcore_hello() on every worker lcore */
-	RTE_LCORE_FOREACH_WORKER(lcore_id) {
-		rte_eal_remote_launch(lcore_hello, NULL, lcore_id);
+	subflow_header gen_header()
+	{
+		uint64_t curr_idx = _counter % 5;
+		subflow_header header;
+		if (curr_idx < 3)
+		{
+			header.subflow_idx = curr_idx;
+			header.seq_num = _counter;
+		}
+		else
+		{
+			header.subflow_idx = curr_idx - 3;
+			header.seq_num = _counter;
+		}
+		_counter += 1;
+		return header;
 	}
 
-	/* call it on main lcore too */
-	lcore_hello(NULL);
+	uint64_t _counter;
+};
 
-	rte_eal_mp_wait_lcore();
+int main()
+{
+	std::cout << "fuck, I need sometime to get used to c++, I'm not quite confident about my current rust implementation" << std::endl;
 
-	/* clean up the EAL */
-	rte_eal_cleanup();
-
-	return 0;
+	subflow_header_generator header_gen = subflow_header_generator();
+	for(int i=0; i<100; i++) {
+		subflow_header header = header_gen.gen_header();
+		std::cout<<"subflow_idx: "<<header.subflow_idx<<", seq_num: "<<header.seq_num<<std::endl;
+	}
 }
